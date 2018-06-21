9a4c861f9a84a91aaa9cc0741fbe1558ce8c852a
isaac <isaac.sennerholt@gmail.com>
2018-06-21 18:49:17 +0200

    Extract authenticateUser function from render

--- src/package-user/userSignUpFormContainer.js
+++ src/package-user/userSignUpFormContainer.js
@@ -8,7 +8,17 @@ export default (UserSignUpService, UserSignUpFormComponent, {UserLoginService} =
     constructor() {
       super()
       this.state = {apiError: {}}
+      this.authenticateUser = this.authenticateUser.bind(this)
       this.setStateOnFailure = this.setStateOnFailure.bind(this)
+    }
+
+    authenticateUser(userLogin, setLocalStorage) {
+      return (undefined, formValues) => {
+        return userLogin(formValues)
+          .then(authSession => {
+            return setLocalStorage(authSession)
+          })
+      }
     }
 
     setStateOnFailure(apiError) {
@@ -16,7 +26,7 @@ export default (UserSignUpService, UserSignUpFormComponent, {UserLoginService} =
     }
 
     render() {
-      const {setStateOnFailure, state: {apiError}} = this
+      const {authenticateUser, setStateOnFailure, state: {apiError}} = this
       const validationSchema = yup.object().shape({
         email: yup.string().email('An email must be formatted as such- example@host.com').required('Please enter a value'),
         password: yup.string().required('Please enter a value').test(
@@ -25,15 +35,6 @@ export default (UserSignUpService, UserSignUpFormComponent, {UserLoginService} =
           password => /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/.test(password)
         )
       })
-  
-      function authenticateUser(userLogin, setLocalStorage) {
-        return (undefined, formValues) => {
-          return userLogin(formValues)
-            .then(authSession => {
-              return setLocalStorage(authSession)
-            })
-        }
-      }
     
       return (
         <div>
